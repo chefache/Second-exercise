04c7aa412ff130b4c23ef8a1d1e613420c7ee455
chefache <stefan.m.koev@gmail.com>
2020-10-22 10:58:14 +0300

    Initialize

---
+++ Apps/SharedTrip/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿namespace SharedTrip.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions db)
+            : base(db)
+        {
+
+        }
+
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=DESKTOP-AMLLGJ5;Database=SharedTrip;Integrated Security=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserTrip>().HasKey(ut => new 
+            {
+                ut.UserId, ut.TripId 
+            });
+        }
+    }
+}
---
+++ Apps/SharedTrip/Data/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SharedTrip.Data
+{
+    public class User
+    {
+        public User()
+        {
+            this.UserTrips = new HashSet<UserTrip>();
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        [Key]
+        [Required]
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Username { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Password { get; set; }
+
+        public virtual IEnumerable<UserTrip> UserTrips { get; set; }
+    }
+}
